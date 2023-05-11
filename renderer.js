1214032b73989f3f71348a6fa3e165c84d6ab707
Aries <hkn406532@163.com>
2023-05-11 15:05:37 +0800

    进程间通信

--- renderer.js
+++ renderer.js
@@ -1,2 +1,10 @@
-const information = document.getElementById('info')
-information.innerText = `This app is using Chrome (v${window.versions.chrome()}), Node.js (v${window.versions.node()}), and Electron (v${window.versions.electron()})`
+
+const func = async () => {
+    const response = await window.versions.ping()
+    console.log(response) // 打印 'pong'
+    const information = document.getElementById('info')
+    information.innerText = `This app is using Chrome (v${window.versions.chrome()}), Node.js (v${window.versions.node()}), and Electron (v${window.versions.electron()}).We receive a message:${response}`
+
+  }
+  
+  func()
