00e82ff242cb96eb9d973837c4b516656af884ea
Aries <hkn406532@163.com>
2023-05-18 15:18:22 +0800

    菜单设置

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ const createWindow = () => {
     width: 800,
     height: 600,
   });
+
   //加载文件
   win.loadFile("index.html");
 
@@ -16,19 +17,116 @@ const createWindow = () => {
   // win.webContents.openDevTools();
 
   // 自定义模板 - 菜单栏
+  // const template = [
+  //   {
+  //     label: "文件",
+  //     submenu: [
+  //       {
+  //         label: "新建"
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     label: "关于",
+  //   },
+  // ];
+
+  const isMac = process.platform === "darwin";
+
   const template = [
+    // { role: 'appMenu' }
+    ...(isMac
+      ? [
+          {
+            label: app.name,
+            submenu: [
+              { role: "about" },
+              { type: "separator" },
+              { role: "services" },
+              { type: "separator" },
+              { role: "hide" },
+              { role: "hideOthers" },
+              { role: "unhide" },
+              { type: "separator" },
+              { role: "quit" },
+            ],
+          },
+        ]
+      : []),
+    // { role: 'fileMenu' }
     {
-      label: "文件",
+      label: "File",
+      submenu: [isMac ? { role: "close" } : { role: "quit" }],
+    },
+    // { role: 'editMenu' }
+    {
+      label: "Edit",
       submenu: [
-        {
-          label: "新建窗口",
-        },
+        { role: "undo" },
+        { role: "redo" },
+        { type: "separator" },
+        { role: "cut" },
+        { role: "copy" },
+        { role: "paste" },
+        ...(isMac
+          ? [
+              { role: "pasteAndMatchStyle" },
+              { role: "delete" },
+              { role: "selectAll" },
+              { type: "separator" },
+              {
+                label: "Speech",
+                submenu: [{ role: "startSpeaking" }, { role: "stopSpeaking" }],
+              },
+            ]
+          : [{ role: "delete" }, { type: "separator" }, { role: "selectAll" }]),
+      ],
+    },
+    // { role: 'viewMenu' }
+    {
+      label: "View",
+      submenu: [
+        { role: "reload" },
+        { role: "forceReload" },
+        { role: "toggleDevTools" },
+        { type: "separator" },
+        { role: "resetZoom" },
+        { role: "zoomIn" },
+        { role: "zoomOut" },
+        { type: "separator" },
+        { role: "togglefullscreen" },
+      ],
+    },
+    // { role: 'windowMenu' }
+    {
+      label: "Window",
+      submenu: [
+        { role: "minimize" },
+        { role: "zoom" },
+        ...(isMac
+          ? [
+              { type: "separator" },
+              { role: "front" },
+              { type: "separator" },
+              { role: "window" },
+            ]
+          : [{ role: "close" }]),
       ],
     },
     {
-      label: "关于",
+      role: "help",
+      submenu: [
+        {
+          label: "Learn More",
+          click: async () => {
+            const { shell } = require("electron");
+            await shell.openExternal("https://electronjs.org");
+          },
+        },
+      ],
     },
   ];
+
   // 编译模板
   const menu = Menu.buildFromTemplate(template);
 
